b5ac95c216d92b7b110433eb957d7e7f56f874f7
Cecilia Valencia <ceci.valencia@outlook.com>
2022-05-28 09:36:48 -0600

    Agregar archivos de proyecto.

---
+++ Examen-PX/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examen_PX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
